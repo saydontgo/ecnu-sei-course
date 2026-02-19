2d733eece3be06f4b7529cfc3c830b487529c62b
zjf <liu358813@gmail.com>
2026-02-19 23:45:04 +0800

    计统

---
+++ 大一下/计统/lab4(1)/10235101477-张建夫-lab4/csim.c
@@ -0,0 +1,145 @@
+#include "cachelab.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<limits.h>
+#define max 100
+int hit = 0, miss = 0, e = 0;
+typedef struct
+{
+    int valid;//有效位
+    int tag;//标记位
+    int time_count;//访问时间计算
+}line;
+typedef struct
+{
+    line* l;
+}set;
+typedef struct
+{
+    int S_count;
+    set* Set;
+    int E;
+}cache;
+void getinfo(int argc, char* argv[], int* s, int* e, int* b, char* filename)//从argv[]中读数据出来，此处用地址传递，以修改main函数中对应变量的值
+{
+    for (int i = 1; i < argc; i += 2)//由于argc[0]是"./test-csim", 没有必要读，所以从1开始                                     //i+=2是因为每个参数前面都有对应的提示符，因此两个两个一组，对于h和v语句，特殊处理即可
+    {
+        switch (argv[i][1])//对应提示符的格式是"-X",所以是读第二个字符
+        {
+        case 's':
+            *s = atoi(argv[i + 1]);
+            break;
+        case 'E':
+            *e = atoi(argv[i + 1]);
+            break;
+        case 't':
+            strcpy(filename, argv[i + 1]);//拿到文件名
+            break;
+        case'b':
+            *b = atoi(argv[i + 1]);
+            break;
+        case'h'://处理h和v语句
+        case'v':
+            i--;
+        default:break;
+        }
+    }
+}
+void initialize_cache(cache* cc, int S, int E)
+{
+    cc->S_count = pow(2, S);
+    cc->E = E;
+    cc->Set = (set*)malloc(cc->S_count * sizeof(set));
+    for (int i = 0; i < cc->S_count; i++) {
+        cc->Set[i].l = (line*)malloc(cc->E * sizeof(line));
+        for (int j = 0; j < cc->E; j++)
+        {
+            cc->Set[i].l[j].valid = 0;
+            cc->Set[i].l[j].tag = 0;
+            cc->Set[i].l[j].time_count = 0;
+        }
+    }
+}
+void cal_hit_miss_evi(char* this_line, cache* c, int B, int S)
+{
+    int hang = c->E;//每组的行数
+    int address;
+    char mode;
+    int matched_set;
+    int longest_time = 0;//初始化被替换行的下标
+    int temp_tag;
+    sscanf(this_line, " %c %x", &mode, &address);
+
+    address >>= B;
+    matched_set = address & (0x7fffffff >> (31 - S));//计算对应的组位置
+    temp_tag = address >> S;//得到标记位
+    for (int i = 0; i < hang; i++) {
+        if (c->Set[matched_set].l[i].valid == 1 && temp_tag == c->Set[matched_set].l[i].tag)//命中
+        {
+            if (mode == 'M')hit++;
+            hit++;
+            for (int j = 0; j < hang; j++)//更新时间
+            {
+                if (c->Set[matched_set].l[j].valid == 1) {
+                    if (i == j) c->Set[matched_set].l[j].time_count = 0;
+                    else c->Set[matched_set].l[j].time_count++;
+                }
+            }
+            return;
+        }
+    }
+    miss++;
+    for (int i = 0; i < hang; i++)
+    {
+        if (c->Set[matched_set].l[i].valid == 0)//遇到空行
+        {
+            c->Set[matched_set].l[i].valid = 1;
+            c->Set[matched_set].l[i].tag = temp_tag;
+            for (int j = 0; j < hang; j++)
+                if (i != j && c->Set[matched_set].l[i].valid == 1)
+                    c->Set[matched_set].l[j].time_count++;
+            if (mode == 'M')hit++;
+            return;
+        }
+    }
+        e++;
+        for (int i = 0; i < hang; i++)
+            if (c->Set[matched_set].l[i].time_count > c->Set[matched_set].l[longest_time].time_count)
+                longest_time = i;//更新被替换行
+        c->Set[matched_set].l[longest_time].tag = temp_tag;//进行替换
+        for (int j = 0; j < hang; j++)
+            if (c->Set[matched_set].l[j].valid == 1)
+                c->Set[matched_set].l[j].time_count++;
+        c->Set[matched_set].l[longest_time].time_count = 0;
+        if (mode == 'M')hit++;
+}
+int main(int argc, char* argv[])
+{
+    int S, E, B;
+    
+    char filename[30];
+    cache cc;
+    getinfo(argc, argv, &S, &E, &B, filename);
+    initialize_cache(&cc, S, E);
+
+    FILE* fp;
+    fp = fopen(filename, "r");//读文件
+    if (fp == NULL)return -1;//文件打开异常则返回
+    char temp_line[max];//每一行的缓冲区
+    while (fgets(temp_line, max, fp) != NULL)//fgets函数读文件的每一行
+    {
+        if (temp_line[0] == 'I')continue;//跳过“I”行
+        cal_hit_miss_evi(temp_line, &cc, B, S);//计算本行的hit,miss,evi
+    }    fclose(fp);
+
+    for (int i = 0; i < cc.S_count; i++)//释放内存
+    {
+        free(cc.Set[i].l);
+    }
+    free(cc.Set);
+    
+    printSummary(hit, miss, e);//按要求调用函数
+    return 0;
+}
---
+++ 大一下/计统/lab5/10235101477--张建夫--lab5/mm-优化版本.c
@@ -0,0 +1,234 @@
+#define WSIZE       4        //字、脚部或头部的大小（字节）
+#define DSIZE       8        //双字大小（字节）
+#define CHUNKSIZE  (1<<12)   //扩展堆时的默认大小
+#define MINBLOCK (DSIZE + 2*WSIZE)
+
+#define MAX(x, y)  ((x) > (y) ? (x) : (y))
+
+#define PACK(size, alloc)  ((size) | (alloc))         //将 size 和 allocated bit 合并为一个字
+
+#define GET(p)        (*(unsigned int *)(p))          //读地址p处的一个字
+#define PUT(p, val)   (*(unsigned int *)(p) = (val))  //写地址p处写一个字
+
+#define GET_SIZE(p)   (GET(p) & ~0x07)    //得到地址p处的 size
+#define GET_ALLOC(p)  (GET(p) & 0x1)      //得到地址p处的有效位（是否分配）
+//block point --> bp指向有效载荷块指针
+#define HDRP(bp)     ((char*)(bp) - WSIZE)                       //获得头部的地址
+#define FTRP(bp)     ((char*)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)  //获得脚部的地址, 与宏定义HDRP有耦合
+
+#define NEXT_BLKP(bp)    ((char*)(bp) + GET_SIZE((char*)(bp) - WSIZE))  //计算后块的地址
+#define PREV_BLKP(bp)    ((char*)(bp) - GET_SIZE((char*)(bp) - DSIZE))  //计算前块的地址
+
+/*
+ * mm-naive.c - The fastest, least memory-efficient malloc package.
+ *
+ * In this naive approach, a block is allocated by simply incrementing
+ * the brk pointer.  A block is pure payload. There are no headers or
+ * footers.  Blocks are never coalesced or reused. Realloc is
+ * implemented directly using mm_malloc and mm_free.
+ *
+ * NOTE TO STUDENTS: Replace this header comment with your own header
+ * comment that gives a high level description of your solution.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+ /*********************************************************
+  * NOTE TO STUDENTS: Before you do anything else, please
+  * provide your team information in the following struct.
+  ********************************************************/
+team_t team = {
+    /* Team name */
+    "ateam",
+    /* First member's full name */
+    "Harry Bovik",
+    /* First member's email address */
+    "bovik@cs.cmu.edu",
+    /* Second member's full name (leave blank if none) */
+    "",
+    /* Second member's email address (leave blank if none) */
+    ""
+};
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+static char* heap_listp;//指向序言快
+
+//增长堆指针
+
+static void* coalesce(void* bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc)
+    {
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+        return bp;
+    }
+    else if (prev_alloc && !next_alloc)
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    else if (!prev_alloc && next_alloc)
+    {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    else
+    {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+//立即合并前后块
+
+static void* extend_heap(size_t words)
+{
+    char* bp;
+    size_t size;
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return coalesce(bp);
+}
+//扩展堆
+
+/*
+ * mm_init - initialize the malloc package.
+ */
+
+int mm_init(void)
+{
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void*)-1)
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+    heap_listp += (2 * WSIZE);
+
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+        return -1;
+    return 0;
+}
+//初始化
+
+static void* find_fit(size_t asize)//首次适配
+{
+    void* bp = heap_listp;
+    while (GET_SIZE(HDRP(bp)) > 0)
+    {
+        if (!GET_ALLOC(HDRP(bp)) && (GET_SIZE(HDRP(bp)) >= asize))
+            return bp;
+        bp = NEXT_BLKP(bp);
+    }
+    return NULL;
+
+}
+
+static void place(void* bp, size_t asize)
+{
+    size_t whole_size = GET_SIZE(HDRP(bp));
+    if ((whole_size - asize) >= (2 * DSIZE))
+    {
+        PUT(HDRP(bp), PACK(asize, 0x1));
+        PUT(FTRP(bp), PACK(asize, 0x1));
+        bp = FTRP(bp) + DSIZE;
+        PUT(HDRP(bp), PACK(whole_size - asize, 0x0));
+        PUT(FTRP(bp), PACK(whole_size - asize, 0x0));
+    }
+    else
+    {
+        PUT(HDRP(bp), PACK(whole_size, 0x1));
+        PUT(FTRP(bp), PACK(whole_size, 0x1));
+    }
+}
+
+
+/*
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+void* mm_malloc(size_t size)
+{
+    size_t asize;
+    size_t extendsize;
+    char* bp;
+    if (size == 0)
+        return NULL;
+    if (size <= DSIZE)
+        asize = 2 * DSIZE;
+    else
+        asize = DSIZE * ((size + (DSIZE)+(DSIZE - 1)) / DSIZE);
+
+    if ((bp = find_fit(asize)) != NULL)
+    {
+        place(bp, asize);
+        return  bp;
+    }
+
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+    return bp;
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void* ptr)
+{
+    size_t size = GET_SIZE(HDRP(ptr));
+
+
+    coalesce(ptr);
+}
+
+/*
+ * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+ */
+void* mm_realloc(void* ptr, size_t size)
+{
+    void* oldptr = ptr;
+    void* newptr;
+    size_t copySize;
+
+    newptr = mm_malloc(size);
+    if (newptr == NULL)
+        return NULL;
+    copySize = *(size_t*)((char*)oldptr - SIZE_T_SIZE);
+    if (size < copySize)
+        copySize = size;
+    memcpy(newptr, oldptr, copySize);
+    mm_free(oldptr);
+    return newptr;
+}
